8ea1463585a2791069ffc637dc06ce639ed24610
LiaozhiCheng <h940664@gmail.com>
2020-10-14 04:44:27 +0800

    後端出始化

---
+++ app/config.py
@@ -0,0 +1,17 @@
+UPLOAD_FOLDER = 'static/images'
+#Flask Security
+DEBUG = True
+SECRET_KEY = 'super-secret'
+SECURITY_PASSWORD_SALT = 'bcrypt'
+SECURITY_LOGIN_USER_TEMPLATE= 'login.html'
+
+
+#MongoDB Config
+MONGODB_HOST = "mongodb+srv://Liao:871029@cluster0-sk2jk.mongodb.net/CramSchool"
+#user_passwordDB
+MONGODB_DB = True
+
+JSON_AS_ASCII = False
+TEMPLATES_AUTO_RELOAD = True
+UPLOAD_FOLDER = UPLOAD_FOLDER
+

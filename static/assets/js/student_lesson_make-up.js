0368c96e33a2b6333e3e2b1f621955009f32fb71
Angel Shau <shauangel2000@gmail.com>
2020-12-17 06:39:03 +0800

    Update student_lesson_make-up.js

--- static/assets/js/student_lesson_make-up.js
+++ static/assets/js/student_lesson_make-up.js
@@ -117,7 +117,7 @@ function fullTd(td_id, date){
 function reservedTd(td_id, c_name, l_name, l_id, date){
     var target = document.getElementById(td_id);
     var display = l_name;
-    if(l_name == ""){ display = l_id; }
+    if(l_name === undefined){ display = l_id; }
     var str = "<a href='#' class='normal' name='" + td_id + '+' + date + "' " 
             + "data-toggle='modal' data-target='#checkoutInfo' " 
             + "onclick='setCheckInfo(this)' id='" + l_id + "'>" 

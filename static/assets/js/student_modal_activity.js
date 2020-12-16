62debf74213373a68c2a801554811ea79043693c
Angel Shau <shauangel2000@gmail.com>
2020-12-17 06:19:17 +0800

    Update student_modal_activity.js

--- static/assets/js/student_modal_activity.js
+++ static/assets/js/student_modal_activity.js
@@ -51,6 +51,9 @@ function getReservation(){
     //取得目標課程&課堂的index
     var courseIdx = document.getElementById("chooseCourse").selectedIndex;
     var lessonIdx = document.getElementById("chooseLesson").selectedIndex;
+    console.log(courseIdx, lessonIdx);
+        console.log(missedCourse);
+        console.log(missedLessonID);
     
     //判斷是否都已選擇
     //未完成選擇，發出提醒
@@ -59,9 +62,6 @@ function getReservation(){
     }
     //完成選擇，傳送資料
     else{
-        console.log(courseIdx, lessonIdx);
-        console.log(missedCourse);
-        console.log(missedLessonID);
         sendReservation(missedCourse[parseInt(courseIdx)-1], missedLessonID[parseInt(courseIdx)-1][parseInt(lessonIdx)-1]);
         var C_Name = document.getElementById("chooseCourse")[courseIdx].text;
         var L_Name = document.getElementById("chooseLesson")[lessonIdx].text;

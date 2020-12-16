0d5a0ce443e0cabe729b37c73b1f46a6ce654897
angel08012000 <58464773+angel08012000@users.noreply.github.com>
2020-12-17 06:40:02 +0800

    Update common.js

--- static/assets/js/common.js
+++ static/assets/js/common.js
@@ -13,7 +13,7 @@ function setSideBar(){
     var content="";
     content += '<div id="dismiss"><i class="fas fa-arrow-left"></i></div><div class="sidebar-header"><h3>CS管理系統</h3></div>';
     
-    content += '<ul class="list-unstyled"><li><a href="cramschool_finish.html">首頁</a></li><li><a href="cramschool_course_finish.html">課程</a></li><li><a href="cramschool_staffstudent_finish.html">成員列表</a></li><li><a href="cramschool_attendence_finish.html">出缺勤</a></li><li><a href="cramschool_makeup_class_finish.html">補課</a></li><li><a href="cramschool_classroom_finish.html">教室</a></li></ul>';
+    content += '<ul class="list-unstyled"><li><a href="'+url_cramschool+'">首頁</a></li><li><a href="'+url_cramschool_course+'">課程</a></li><li><a href="'+url_cramschool_staffstudent+'">成員列表</a></li><li><a href="'+url_cramschool_attendence+'">出缺勤</a></li><li><a href="'+url_cramschool_makeup_class+'">補課</a></li><li><a href="'+url_cramschool_classroom+'">教室</a></li></ul>';
     
     obj.innerHTML=content;
 }

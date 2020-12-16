23bb2200983e8179e59c4b5860dc0b4e01f4c6e0
Liao <h940664@gmail.com>
2020-12-16 23:32:53 +0800

    fix_merge
    fix models/user.py missing content and change db->_db

--- models/_db.py
+++ models/_db.py
@@ -11,6 +11,15 @@ from pymongo import MongoClient
 
 DB = MongoClient('mongodb+srv://Liao:871029@cluster0-sk2jk.mongodb.net')['CramSchool']
 
-STUDENT_COLLECTION = DB['student']
-TEACHER_COLLECTION = DB['teacher']
-BOSS_COLLECTION = DB['boss']
+
+USER_DATA_COLLECTION = DB['User']
+
+USER_COLLECTION=DB['User']
+
+COURSE_COLLECTION=DB['Course']
+
+CLASSROOM_COLLECTION=DB['Classroom']
+
+RESCHEDULE_COLLECTION=DB['Reschedule']
+
+LESSON_COLLECTION=DB['Lesson']
--- models/lesson.py
+++ models/lesson.py
@@ -6,13 +6,9 @@ Created on Sun Dec 13 15:45:27 2020
 @author: linxiangling
 """
 
-from bson.objectid import ObjectId
-from models import db
 
-from flask_bcrypt import Bcrypt
+from models import _db
 import sys
-
-from datetime import datetime,timezone,timedelta
 
 
 sys.path.insert(0, './models')
@@ -20,13 +16,13 @@ sys.path.insert(0, './models')
     
 #課程資訊(出缺席紀錄)
 def get_lesson_info(lesson_id):
-    return db.LESSON_COLLECTION.find_one({'lesson_id':lesson_id})
+    return _db.LESSON_COLLECTION.find_one({'lesson_id':lesson_id})
       
 
 #lesson列表(出缺席紀錄，依student) 
 def get_lesson_list(course_id):
-    return db.COURSE_COLLECTION.find_one({'course_id':course_id})['lesson_list']
+    return _db.COURSE_COLLECTION.find_one({'course_id':course_id})['lesson_list']
 
 #編輯lesson資訊(編輯出缺席)
 def update_lesson_attendence_info(lessonid, attendence_list):
-    db.LESSON_COLLECTION.update_one(lessonid, {'$set':{'attendence':attendence_list}})
+    _db.LESSON_COLLECTION.update_one(lessonid, {'$set':{'attendence':attendence_list}})

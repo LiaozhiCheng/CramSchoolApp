14a713ae962425d8caaada8922914eb58f200149
Liao <h940664@gmail.com>
2020-12-17 00:42:49 +0800

    change host

--- main.py
+++ main.py
@@ -28,6 +28,6 @@ def create_app():
 
 if __name__ == "__main__":
     app = create_app()
-    app.run("192.168.111.255",port=8080)
+    app.run("192.168.111.128",port=55023)
     
 #"140.121.197.130",port=8080

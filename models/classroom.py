383e653c4a14a881fc3545fa783f9e3bd2a35029
xxxPatty <58462537+xxxPatty@users.noreply.github.com>
2020-12-17 05:08:14 +0800

    Update classroom.py

--- models/classroom.py
+++ models/classroom.py
@@ -6,9 +6,13 @@ Created on Sun Dec 13 15:46:25 2020
 @author: linxiangling
 """
 
+from bson.objectid import ObjectId
 from models import _db
+
+from flask_bcrypt import Bcrypt
 import sys
 
+from datetime import datetime,timezone,timedelta
 
 
 sys.path.insert(0, './models')
@@ -16,24 +20,24 @@ sys.path.insert(0, './models')
 
 #教室資訊(classroom_id)
 def get_classroom_info(classroom_id):
-    data = _db.CLASSROOM_COLLECTION.find_one({'classroom_id':classroom_id})     #不能直接return會有bson問題
+    data=_db.CLASSROOM_COLLECTION.find_one({'classroom_id':classroom_id})     #不能直接return會有bson問題
     return {'classroom_id':data['classroom_id'], 'name':data['name'], 'capacity':data['capacity']}
 
 #教室列表
 def get_classroom_list():
-    return [{'classroom_id':i['classroom_id'], 'name':i['name'], 'capacity':i['capacity']} for i in _db.CLASSROOM_COLLECTION.find()]
+    return [{'classroom_id':i['classroom_id'], 'name':i['name'], 'capacity':i['capacity']}for i in _db.CLASSROOM_COLLECTION.find()]
 
 #教室資訊(name)
 def get_classroom_info_by_name(name):
-    data = _db.CLASSROOM_COLLECTION.find_one({'name':name})     #不能直接return會有bson問題
+    data=_db.CLASSROOM_COLLECTION.find_one({'name':name})     #不能直接return會有bson問題
     return {'classroom_id':data['classroom_id'], 'name':data['name'], 'capacity':data['capacity']}
 
 
 #新增教室資訊
 def insert_classroom(name, capacity):
     #用資料庫筆數當id後綴，會有問題，之後改用static變數一直往上累計
-    classroom_id = "R-"+str(_db.CLASSROOM_COLLECTION.count_documents({})+1).zfill(3)
-    classroomdict = {'classroom_id':classroom_id, 'name':name, 'capacity':capacity}
+    classroom_id="R-"+str(_db.CLASSROOM_COLLECTION.count_documents({})+1).zfill(3)
+    classroomdict={'classroom_id':classroom_id, 'name':name, 'capacity':capacity}
     _db.CLASSROOM_COLLECTION.insert_one(classroomdict)
     
 #刪除教室資訊
@@ -43,8 +47,3 @@ def delete_classroom(classroomid):
 #編輯教室資訊
 def update_classroom(classroomid, classroomdict):
     _db.CLASSROOM_COLLECTION.update_one(classroomid, {'$set':classroomdict})
-    
-# 依據 user_id 找單一物件
-def get_by_classroomid(classroom_id):
-    item = _db.CLASSROOM_COLLECTION.find_one({'classroom_id' : classroom_id})
-    return item

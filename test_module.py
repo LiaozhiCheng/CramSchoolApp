7f3440d68a103baa31216e3a196f5b3d58cd52cd
Liao <h940664@gmail.com>
2021-01-10 04:37:18 +0800

    新增單元測試的說明

--- test_module.py
+++ test_module.py
@@ -26,27 +26,34 @@ class SettingBase(TestCase):
 指令說明
 cmd 輸入pip install Flask-Testing
 cmd 執行python test_module.py即可測試
-
+測試的class要繼承SettingBase
+測試的func開頭要是test_
+response相關指令可參考https://pythonhosted.org/Flask-Testing/
+url_for是指哪個blueprint.哪個function(不是網址)
 
 
 """
 #測試登入
 class CheckUserAndLogin(SettingBase):
     def test_signup(self):
-        response = self.signup
-        self.assertEqual(response.status_code, 200)
+
+        response = self.signup()                           
+        self.assert200(response)
         
         
         
-#測試teacher api
+#測試teacher_api
 #Todo
 class CheckTeacherAPI(SettingBase):
     def test_course_communication_book(self):
         response = self.client.get(url_for('teacher_api.course_communication_book'))
         self.assertEquals(response.json, list())
-        return response
 
 
+#測試student_api
+#Todo
+class CheckStudentAPI(SettingBase):
+    pass
 
 
 

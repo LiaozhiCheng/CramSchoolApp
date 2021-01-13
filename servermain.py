622c717337799f84c1245b30e4497b05862bf939
Liao <h940664@gmail.com>
2021-01-13 23:56:46 +0800

    fix bug

---
+++ servermain.py
@@ -0,0 +1,41 @@
+from flask import Flask
+from flask_apscheduler import APScheduler
+from flask_security import Security
+from flask_cors import CORS
+import models
+from views import register_blueprint
+from lib import config
+
+
+
+def create_app():
+    app = Flask(__name__)
+    app.jinja_env.auto_reload = True
+    app.config.from_object(config.Config())
+    CORS(app)
+    # models setup
+    models.setup(app)
+
+    # security setup
+
+    Security(app, models.user.USER_DATASTORE,login_form=models.user.ExtendedLoginForm)
+
+    # register app
+    register_blueprint(app)
+    return app
+
+
+def refresh_schedule():
+    models.reschedule.refresh_schedule()
+
+
+
+
+if __name__ == "__main__":
+    # scheduler=APScheduler()
+    app = create_app()
+    # scheduler.init_app(app)
+    # scheduler.start()
+    app.run("192.168.111.128",port=55001)
+    
+#"192.168.111.128",port=55001

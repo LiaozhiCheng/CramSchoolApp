15f5e663f94ca3890ebd37bc4d4a1f967505cb45
Chinger-Chang <an990218@gmail.com>
2021-01-14 00:20:29 +0800

    排版和modal編輯

--- static/assets/js/communication_book.js
+++ static/assets/js/communication_book.js
@@ -5,14 +5,10 @@
                     getSavedData();
                     setSideBar();
                 }
-                    function getSavedData(){
-                        
+                function getSavedData(){
+                    
                     try{
                         temp=sessionStorage.getItem('course');
-                        //console.log("sss");
-                        //settemp();
-                        //console.log("qqq");
-                        //var temp = sessionStorage.getItem('course');
                         console.log(temp);
                         if(temp==null){
                             alert("未知課程，請回課表選擇課程");
@@ -20,7 +16,7 @@
                         }
 
                         $.ajax({
-                        url: api_communication_book + temp,
+                            url: api_communication_book + temp,
                         //url: "cb.json", //放你的url，這裡先放本地端檔案
                         type: "GET",
                         dataType: "json",
@@ -31,11 +27,11 @@
                             setSchedule(data);
                             for(var i=0; i<data.length; i++){
                                 //利用key去找value
-                                //console.log("time: "+data[i].lesson_time);
-                                //console.log("time: "+data[i].lesson_id);
-                                //console.log("deadline: "+data[i].deadline);
-                                //console.log("progress: "+data[i].progress);
-                                //console.log("context: "+data[i].context);
+                                console.log("time: "+data[i].lesson_time);
+                                console.log("id: "+data[i].lesson_id);
+                                console.log("deadline: "+data[i].deadline);
+                                console.log("progress: "+data[i].progress);
+                                console.log("context: "+data[i].context);
                             }//看到時候有沒有成功
                         },
 
@@ -51,50 +47,30 @@
                     }
                     
                 }
-                /*function settemp(){
-                    console.log("qqq");
-                    var temp = sessionStorage.getItem('course');}*/
-                        function setSchedule(data){
-                            var t = document.getElementById("cbtable").getElementsByTagName("tbody");
-                            var rows = "";
-                            for(var i=0; i<data.length; i++){
-                                //利用key去找value
-                                lessonid[i]=data[i].lesson_id;
-                                date[i]=data[i].lesson_time;
-                                console.log("deadline: "+data[i].deadline);
-                                //console.log("progress: "+data[i].progress);
-                                //console.log("context: "+data[i].context);
-                            }
-                           for(var i=0; i<date.length; i++){
-                                    rows += "<tr><td>" + date[i] + "</td>";
-                                    //for(var j=0; j<4; j++){if(j==3){
-                                            rows += "<td id='" + i + "-0'>"+data[i].deadline+"</td>";
-                                            rows += "<td id='" + i + "-1'>"+data[i].progress+"</td>";
-                                            rows += "<td id='" + i + "-2'>"+data[i].context+"</td>";
-                                            rows += "<td id='" + i + "-3'><button id='e"+ i +"' type='button' class='btn btn-primary' data-toggle='modal' data-target='#exampleModal' onclick='testedit(this.id)' >Edit</button><button id='d"+ i +"' type='button' class='btn btn-outline-danger' onclick='testdelete(this.id)'>Delete</button></td>";
-                                            //}
-                                            //else{
-                                            //rows += "<td id='" + i + "-" + j + "'></td>";}
-                                    //}
-                                    rows += "</tr>"
-                            }
-                            t[0].innerHTML = rows;
-                        }
-                        //function test(){
-                          //  document.getElementById("0-0").innerHTML = "國文";
-                        //}
-                        /*$('#exampleModal').click(function() {
-                            //console.log($(this).attr('id'));
-                            $('#1-2').text($('#context').val());
-                            $('#1-1').text($('#progress').val());
-                            $('#1-0').text($('#deadline').val());
-                        });*/
+                function setSchedule(data){
+                    var t = document.getElementById("cbtable").getElementsByTagName("tbody");
+                    var rows = "";
+                    for(var i=0; i<data.length; i++){
+                        lessonid[i]=data[i].lesson_id;
+                        date[i]=data[i].lesson_time;
+                        console.log("deadline: "+data[i].deadline);
+                    }
+                    for(var i=0; i<date.length; i++){
+                        rows += "<tr><td>" + date[i] + "</td>";
+                        rows += "<td id='" + i + "-0'>"+data[i].deadline+"</td>";
+                        rows += "<td id='" + i + "-1'>"+data[i].progress+"</td>";
+                        rows += "<td id='" + i + "-2'>"+data[i].context+"</td>";
+                        rows += "<td id='" + i + "-3'><button id='e"+ i +"' type='button' class='btn btn-primary' data-toggle='modal' data-target='#exampleModal' onclick='testedit(this.id)' >Edit</button><button id='d"+ i +"' type='button' class='btn btn-outline-danger' onclick='testdelete(this.id)'>Delete</button></td>";
+                        rows += "</tr>"
+                    }
+                    t[0].innerHTML = rows;
+                }
 
-                       function returndata(submitdeadline,submitprogress,submitcontext) {
-                            console.log(ss);
-                            var cb = { "lesson_id" : lessonid[ss] , "lesson_time" : date[ss]  , "deadline" : submitdeadline , "progress" : submitprogress, "context" : submitcontext };
-                            console.log(cb);
-                            $.ajax({
+                function returndata(submitdeadline,submitprogress,submitcontext) {
+                    console.log(ss);
+                    var cb = { "lesson_id" : lessonid[ss] , "lesson_time" : date[ss]  , "deadline" : submitdeadline , "progress" : submitprogress, "context" : submitcontext };
+                    console.log(cb);
+                    $.ajax({
                         url: api_edit_communication_book,
                         type: "POST",
                         data: JSON.stringify(cb),
@@ -103,7 +79,7 @@
                         
                         success: function(data){
                             
-                            
+                            console.log(data);
                                 //利用key去找value
                                 if(data.message == "資料不得為空")
                                 {
@@ -114,40 +90,38 @@
                                     alert("send success!");
                                 }
 
-                            getSavedData();
+                                getSavedData();
+                                
+                            },
                             
-                        },
-                        
-                        error: function(){
-                            alert("send error!!!");
-                        }
-                    });
+                            error: function(){
+                                alert("send error!!!");
+                            }
+                        });
                 } 
-                        function submit() {
+                function submit() {
                             //console.log(ss);
                             var submitdeadline = document.getElementById("deadline").value;
                             var submitprogress = document.getElementById("progress").value;
                             var submitcontext = document.getElementById("context").value;
                             returndata(submitdeadline,submitprogress,submitcontext);
-                            //document.getElementById(ss+"-0").innerHTML=submitdeadline;
-                           //document.getElementById(ss+"-1").innerHTML=submitprogress;
-                            //document.getElementById(ss+"-2").innerHTML=submitcontext;
-                }
-                $('#submit').click(function() {
-                        $('#exampleModal').modal('hide');
-                });
+                        }
+                        $('#submit').click(function() {
+                            $('#exampleModal').modal('hide');
+                            $('#progress').val("");
+                            $('#context').val("");
+                            $('#deadline').val("");
+                        });
                         function testedit(clickedid) {
-                     
-                    ss=clickedid.slice(1,2);
-                }
+                           
+                            ss=clickedid.slice(1,2);
+                        }
 
-                function testdelete(clickedid) {
-                     //var NewStringValue=document.getElementById("message-text").value;
-                    ss=clickedid.slice(1,2);
-                    console.log(lessonid[ss]);
-                    //returndata("","","");
-                     $.ajax({
-                        url: api_delete_communication_book + lessonid[ss],
+                        function testdelete(clickedid) {
+                            ss=clickedid.slice(1,2);
+                            console.log(lessonid[ss]);
+                            $.ajax({
+                                url: api_delete_communication_book + lessonid[ss],
                         //url: "cb.json", //放你的url，這裡先放本地端檔案
                         type: "GET",
                         dataType: "json",
@@ -155,6 +129,7 @@
 
                         success: function(data){//這裡拿到的data是一個Object陣列
                             console.log("success");
+                            console.log(data);
                             getSavedData();
                             //看到時候有沒有成功
                         },
@@ -167,32 +142,4 @@
                     //document.getElementById(ss+"-1").innerHTML="";
                     //document.getElementById(ss+"-2").innerHTML="";
                 }
-                      /*  $('#0').click(function() {"d"+
-                            $('#0-0').text("sstt");
-                        });*/
-                   /* $.ajax({
-                        
-                        url: "cb.json", //放你的url，這裡先放本地端檔案
-                        type: "GET",
-                        dataType: "json",
-                        contentType: 'application/json; charset=utf-8',
-                        
-                        //如果成功的話
-                        success: function(data){//這裡拿到的data是一個Object陣列
-                            console.log("success");
-                            setSchedule(data);
-                            for(var i=0; i<data.length; i++){
-                                //利用key去找value
-                                console.log("time: "+data[i].lesson_time);
-                                console.log("time: "+data[i].lesson_id);
-                                console.log("deadline: "+data[i].deadline);
-                                console.log("progress: "+data[i].progress);
-                                console.log("context: "+data[i].context);
-                            }//看到時候有沒有成功
-                        },
-                        
-                        //如果失敗的話
-                        error: function(){
-                            console.log("error");
-                        }
-                    });*/
+                
--- static/assets/js/teacher.js
+++ static/assets/js/teacher.js
@@ -1,149 +1,110 @@
-        var courseid=0;
-        var courseidArray= new Array();
-        window.onload = function init(){
-                    setSchedule();
-                    setSideBar();
-                    $.ajax({
-                
-                url: api_schedule, //放你的url，這裡先放本地端檔案
-                //url: "teacher.json",
-                type: "GET",
-                dataType: "json",
-                contentType: 'application/json; charset=utf-8',
-                
-                //如果成功的話
-                success: function(data){//這裡拿到的data是一個Object陣列
-                    console.log("success");
-                    test(data);
-                    for(var i=0; i<data.length; i++){
-                        //利用key去找value
-                        
-                        console.log("time: "+data[i].time.slice(0,2));
-                        console.log("name: "+data[i].course);
-                    }//看到時候有沒有成功
-                },
-                
-                //如果失敗的話
-                error: function(){
-                    console.log("error");
-                }
-            });
-                    //localStorage.clear();
-                    //setInfo();
-                    //test();
-                    $.ajax({
-                
-               url: api_personal_info, //放你的url，這裡先放本地端檔案
-                 //url: "info.json",
-                type: "GET",
-                dataType: "json",
-                contentType: 'application/json; charset=utf-8',
-                
-                //如果成功的話
-                success: function(data){//這裡拿到的data是一個Object陣列
-                    console.log("success");
-                    setInfo(data);
-                    //for(var i=0; i<data.length; i++){
-                        //利用key去找value
-                        console.log("time: "+data.user_id);
-                        console.log("name: "+data.name);
-                        console.log("phone: "+data.phone);
-                        console.log("email: "+data.email);
-                        console.log("email: "+data.major);
-                    //}//看到時候有沒有成功
-                },
-                
-                //如果失敗的話
-                error: function(){
-                    console.log("error");
-                }
-            });
-                }
-                function setInfo(data){
-                   //for(var i=0; i<data.length; i++){
-                        //利用key去找value
-                        document.getElementById("teacherinfo").innerHTML= "<h2>帳號: " + data.user_id + "</h2><h2>姓名: " + data.name + "</h2><h2>電話: " + data.phone + "</h2><h2>信箱: " + data.email + "</h2><h2>專業: " + data.major + "</h2>";
-                        //console.log("time: "+data[i].user_id);
-                        //console.log("name: "+data[i].name);
-                        //console.log("phone: "+data[i].phone);
-                        //console.log("email: "+data[i].email);
-                    //}
+            var courseid=0;
+            var courseidArray= new Array();
+            window.onload = function init(){
+                        setSchedule();
+                        setSideBar();
+                        $.ajax({
                     
+                    url: api_schedule, //放你的url，這裡先放本地端檔案
+                    //url: "teacher.json",
+                    type: "GET",
+                    dataType: "json",
+                    contentType: 'application/json; charset=utf-8',
+                    
+                    //如果成功的話
+                    success: function(data){//這裡拿到的data是一個Object陣列
+                        console.log("success");
+                        test(data);
+                        for(var i=0; i<data.length; i++){
+                            //利用key去找value
                             
-                    //s.append(rows);ss
-                }
-                function setCourseID(id){
-                   // var idd=document.getElementById(id).value;
-                    //console.log("id: "+id);
-                    //var idd=courseidArray[document.getElementById(id).value];
-                    //console.log("id: "+idd);
-
-                    //courseid=courseidArray[id];
-                    //console.log(courseid);
-                    sessionStorage.setItem("course",id);
-        }
-
-                function setSchedule(){
-                    var t = document.getElementById("teacherschedule").getElementsByTagName("tbody");
-                    var rows = "";
-                    var time = [ "上午" , "下午" , "晚上" ];
-                    var week = ["Mon" , "Tue" , "Wed" , "Thr" , "Fri" , "Sat" , "Sun"];
-                    for(var i=0; i<3; i++){
-                            rows += "<tr><td>" + time[i] + "</td>";
-                            for(var j=0; j<7; j++){
-                                    rows += "<td id='" + i + "-" + week[j] + "'></td>";
-                            }
-                            rows += "</tr>"
-                    }
-                    t[0].innerHTML = rows;
-                }
-
-                function test(data){
+                            console.log("time: "+data[i].time.slice(0,2));
+                            console.log("name: "+data[i].course);
+                        }//看到時候有沒有成功
+                    },
                     
-                    for(var k=0; k<data.length; k++){
-                       courseidArray[k]=data[k].course_id;
-                       console.log(courseidArray[k]);
-                   }
+                    //如果失敗的話
+                    error: function(){
+                        console.log("error");
+                    }
+                });
+                        //localStorage.clear();
+                        //setInfo();
+                        //test();
+                        $.ajax({
                     
-                    for(var i=0; i<data.length; i++){
-
-                       //courseidArray[i]=data[i].course_id;
-                        //console.log(courseidArray[i]);
-                    if(data[i].time.slice(0,2) < 12){
-                        //document.getElementById("0-"+ data[i].time.slice(-3)).value=i;
-                        //console.log(document.getElementById("0-"+ data[i].time.slice(-3)).value);
-                        //courseid=document.getElementById("0-"+ data[i].course_time.slice(-3)).value;
-                        //console.log(courseidArray[document.getElementById("0-"+ data[i].course_time.slice(-3)).value]);
-                        //console.log(courseidArray[2]);
-                        //onclick='setCourseID(courseidArray[document.getElementById("0-"+ data[i].course_time.slice(-3)).value])'
-                        var tempID = courseidArray[i];
-                        //console.log("this.id: "+"0-"+ data[i].course_time.slice(-3));<a href="test2.html"
-                        document.getElementById("0-"+ data[i].time.slice(-3)).innerHTML += '<a href="'+url_teacher_courseinfo+'" id="' + courseidArray[i] + '" onclick="setCourseID('+"'"+tempID+"'"+')" >'+data[i].time.slice(0,11)+"<br>"+data[i].course+"</a>";
-                        
-                        
-                    }
-                    else if(data[i].time.slice(0,2) > 12 && data[i].time.slice(0,2) < 18){
-                        
-                        //document.getElementById("1-"+ data[i].time.slice(-3)).value=i;
-                        var tempID = courseidArray[i];
-                        //console.log(document.getElementById("1-"+ data[i].time.slice(-3)).value);
-                        console.log(tempID);
-                        document.getElementById("1-"+ data[i].time.slice(-3)).innerHTML += '<a href="'+url_teacher_courseinfo+'" id="' + courseidArray[i] + '" onclick="setCourseID('+"'"+tempID+"'"+')" >'+data[i].time.slice(0,11)+"<br>"+data[i].course+"</a>";
-                    }
-                    else {
-                        //document.getElementById("2-"+ data[i].time.slice(-3)).value=i;
-                        //console.log(document.getElementById("2-"+ data[i].time.slice(-3)).value);
-                        //courseid=courseidArray[document.getElementById("2-"+ data[i].course_time.slice(-3)).value];
-                        var tempID = courseidArray[i];
-                        document.getElementById("2-"+ data[i].time.slice(-3)).innerHTML += '<a href="'+url_teacher_courseinfo+'" id="' + courseidArray[i] + '" onclick="setCourseID('+"'"+tempID+"'"+')" >'+data[i].time.slice(0,11)+"<br>"+data[i].course+"</a>";
-                    }
-                    //weekArray[i]=data[i].course_time.slice(-3);
+                   url: api_personal_info, //放你的url，這裡先放本地端檔案
+                     //url: "info.json",
+                    type: "GET",
+                    dataType: "json",
+                    contentType: 'application/json; charset=utf-8',
                     
+                    //如果成功的話
+                    success: function(data){//這裡拿到的data是一個Object陣列
+                        console.log("success");
+                        setInfo(data);
+                            console.log("time: "+data.user_id);
+                            console.log("name: "+data.name);
+                            console.log("phone: "+data.phone);
+                            console.log("email: "+data.email);
+                            console.log("email: "+data.major);
+                    },
+                    
+                    //如果失敗的話
+                    error: function(){
+                        console.log("error");
                     }
-                    //console.log(newArray[0]);
-                    //document.getElementById("0-"+ data[1].course_time.slice(-3)).innerHTML = "<a href='test2.html' >09:00-12:00 國文</a>";
-                }
+                });
+                    }
+                    function setInfo(data){
+                            document.getElementById("teacherinfo").innerHTML= "<h2>帳號: " + data.user_id + "</h2><h2>姓名: " + data.name + "</h2><h2>電話: " + data.phone + "</h2><h2>信箱: " + data.email + "</h2><h2>專業: " + data.major + "</h2>";
+                    }
+                    function setCourseID(id){
+                        sessionStorage.setItem("course",id);
+            }
 
-                
+                    function setSchedule(){
+                        var t = document.getElementById("teacherschedule").getElementsByTagName("tbody");
+                        var rows = "";
+                        var time = [ "上午" , "下午" , "晚上" ];
+                        var week = ["Mon" , "Tue" , "Wed" , "Thr" , "Fri" , "Sat" , "Sun"];
+                        for(var i=0; i<3; i++){
+                                rows += "<tr><td>" + time[i] + "</td>";
+                                for(var j=0; j<7; j++){
+                                        rows += "<td id='" + i + "-" + week[j] + "'></td>";
+                                }
+                                rows += "</tr>"
+                        }
+                        t[0].innerHTML = rows;
+                    }
 
-                
+                    function test(data){
+                        
+                        for(var k=0; k<data.length; k++){
+                           courseidArray[k]=data[k].course_id;
+                           console.log(courseidArray[k]);
+                       }
+                        
+                        for(var i=0; i<data.length; i++){
+                        if(data[i].time.slice(0,2) < 12){
+                            var tempID = courseidArray[i];
+                            document.getElementById("0-"+ data[i].time.slice(-3)).innerHTML += '<a href="'+url_teacher_courseinfo+'" id="' + courseidArray[i] + '" onclick="setCourseID('+"'"+tempID+"'"+')" >'+data[i].time.slice(0,11)+"<br>"+data[i].course+"</a>";
+                            
+                            
+                        }
+                        else if(data[i].time.slice(0,2) > 12 && data[i].time.slice(0,2) < 18){
+                            var tempID = courseidArray[i];
+                            console.log(tempID);
+                            document.getElementById("1-"+ data[i].time.slice(-3)).innerHTML += '<a href="'+url_teacher_courseinfo+'" id="' + courseidArray[i] + '" onclick="setCourseID('+"'"+tempID+"'"+')" >'+data[i].time.slice(0,11)+"<br>"+data[i].course+"</a>";
+                        }
+                        else {
+                           
+                            var tempID = courseidArray[i];
+                            document.getElementById("2-"+ data[i].time.slice(-3)).innerHTML += '<a href="'+url_teacher_courseinfo+'" id="' + courseidArray[i] + '" onclick="setCourseID('+"'"+tempID+"'"+')" >'+data[i].time.slice(0,11)+"<br>"+data[i].course+"</a>";
+                        }
+                        }
+                    }
+
+                    
+
+                    

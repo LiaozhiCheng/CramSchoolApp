a43422c0e17035bfadea72c61649a3ab8a15c845
Liao <h940664@gmail.com>
2021-01-13 22:31:23 +0800

    change to server mode

--- main.py
+++ main.py
@@ -36,6 +36,6 @@ if __name__ == "__main__":
     app = create_app()
     # scheduler.init_app(app)
     # scheduler.start()
-    app.run()
+    app.run("192.168.111.128",port=55001)
     
 #"192.168.111.128",port=55001

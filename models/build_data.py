a1110e32a6409430a904ccaf5a31b1943bc0c247
Liao <h940664@gmail.com>
2021-01-21 21:41:04 +0800

    changeServer

--- models/build_data.py
+++ models/build_data.py
@@ -4,8 +4,6 @@ from models import _db, user
 
 def add_users():
     for the_user in _db.USER_DATA_COLLECTION.find():
-        if _db.USER_COLLECTION.find({'user_id':the_user['user_id']}) is not None:
-            continue
         print(the_user)
         try:
             user.create_user(the_user['name'], the_user['password'], the_user['phone'], the_user['user_id'], the_user['email'], the_user['role'], the_user['major'], the_user['course_list'], [])
